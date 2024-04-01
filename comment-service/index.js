d0a965a601335b911af64c03746f5c89490b1f5b
vichet vatt <vichetvattedu@gmail.com>
2024-04-01 12:15:06 +0700

    Added Event-Bus and post-comment-query service

--- comment-service/index.js
+++ comment-service/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
+
 const { randomBytes } = require("crypto");
 const app = express();
 app.use(bodyParser.json());
@@ -10,15 +12,29 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostID[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentID = randomBytes(4).toString("hex");
-  // console.log(req.body);
   const { commentText } = req.body;
   const comments = commentsByPostID[req.params.id] || [];
   const body = { id: commentID, commentText };
   comments.push(body);
   commentsByPostID[req.params.id] = comments;
+
+  const result = await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentID,
+      commentText,
+      postId: req.params.id,
+    },
+  });
+  console.log(result);
   res.status(201).send(body);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event: Post Created");
+  res.send({});
 });
 
 app.listen(4001, () => {
--- post-service/index.js
+++ post-service/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const axios = require("axios");
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,7 +12,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = {
@@ -18,7 +20,20 @@ app.post("/posts", (req, res) => {
     title,
   };
 
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Event Received:", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
